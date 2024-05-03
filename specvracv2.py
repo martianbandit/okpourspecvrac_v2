10d1b0dff2f6dcbfcdbe06bbe7edc86f5f6ca092
Jo Paradis <154098629+martianbandit@users.noreply.github.com>
2024-05-02 22:50:11 -0400

    Update specvracv2.py

--- specvracv2.py
+++ specvracv2.py
@@ -2,4 +2,4 @@ import streamlit as st
 import openai
 
 
-st.title("Bienvenue sur l'application de chat IA de Le Spéc:rainbow['IA']liste du Vrac
+st.title("Bienvenue sur l'application de chat IA de Le Spéc:rainbow['IA']liste du Vrac")
